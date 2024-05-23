0c7d2c58691e4d4a92717c960cae6c3035b85fc6
ldhwaddell <ldhwaddell@gmail.com>
2024-05-22 22:11:25 -0300

    add logic for running the manager

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+use std::process;
+use traffic_manager::arguments::Arguments;
+use traffic_manager::manager::Manager;
+
 fn main() {
-    println!("Hello, world!");
+    // Parse the arguments
+    let args = Arguments::parse();
+
+    // Validate args
+    let args = Arguments::validate(&args).unwrap_or_else(|err| {
+        eprintln!("Problem validating arguments: {err}");
+        process::exit(1)
+    });
+
+    // Build the manager based on args
+    let manager = Manager::new(&args);
+
+    // Run the manager
+    manager.start().unwrap_or_else(|err| {
+        eprintln!("Problem running manager: {err}");
+        process::exit(1)
+    });
 }
