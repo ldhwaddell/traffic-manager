0a141a97a065e68280900c09d865c0d9d12272c2
ldhwaddell <ldhwaddell@gmail.com>
2024-05-22 22:12:53 -0300

    add lib

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+// CLI args
+pub mod arguments;
+
+// Manager process
+pub mod manager;
+
+// Bus process
+pub mod bus;
+
+// Junction struct with accessors
+pub mod junction;
+
+// Graph struct with accessors
+pub mod graph;
