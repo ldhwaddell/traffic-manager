9bcccc62d59eae64c1d8881530dee5a3503854ac
ldhwaddell <ldhwaddell@gmail.com>
2024-05-23 20:14:20 -0300

    add deadlock check, check for thread completion

--- src/manager.rs
+++ src/manager.rs
@@ -1,14 +1,12 @@
+use crate::arguments::Arguments;
+use crate::bus::Bus;
+use crate::graph::Graph;
+use crate::junction::Junction;
 use rand::Rng;
 use std::error::Error;
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
-// use petgraph::Graph;
-use crate::bus::Bus;
-use crate::graph::Graph;
-use crate::junction::Junction;
-
-use crate::arguments::Arguments;
 
 pub struct Manager {
     pub probability: f32,
@@ -39,37 +37,39 @@ impl Manager {
             .enumerate()
             .map(|(i, c)| Bus::new(i, c));
 
+        // Keep track of thread handles
+        let mut thread_handles = Vec::new();
+
         'outer: loop {
             // Probability p of checking for deadlock
             if rng.gen_range(0.0..1.0) < self.probability {
                 println!("Checking for deadlock");
                 // if deadlock return error
-                // self.graph.check_deadlock()?;
-
-
+                self.graph.check_deadlock()?;
             } else if let Some(bus) = buses.next() {
                 // Clone mutex for graph
-                bus.start(&self.junction, &self.graph);
+                let handle = bus.start(&self.junction, &self.graph);
+                thread_handles.push(handle);
             } else {
                 // Enter main loop to check for deadlock once a second
-                let mut i = 0;
-
                 loop {
                     println!("Periodically checking for deadlock...");
-                    thread::sleep(Duration::from_secs(1));
-                    i = i + 1;
-                    // println!("i: {}", i);
+                    self.graph.check_deadlock()?;
 
-
-                    if i == 5 {
-                        // if deadlock return error
-                        println!("Breaking outer");
+                    // Check if all threads have finished
+                    if thread_handles.iter().all(|handle| handle.is_finished()) {
+                        println!("ALL THREADS FINISHED ");
                         break 'outer;
                     }
+
+                    thread::sleep(Duration::from_secs(1));
                 }
             }
-
             thread::sleep(Duration::from_secs(1));
+        }
+
+        for handle in thread_handles {
+            handle.join().unwrap();
         }
 
         Ok(())
